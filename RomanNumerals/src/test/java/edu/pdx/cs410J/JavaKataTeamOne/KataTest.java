d80e06939460c5d98efff5449fd10aa0d3299105
Chad Tolleson <tolleson@pdx.edu>
2020-07-08 19:52:08 -0700

    added initial Roman Numeral Kata files

--- RomanNumerals/src/test/java/edu/pdx/cs410J/JavaKataTeamOne/KataTest.java
+++ RomanNumerals/src/test/java/edu/pdx/cs410J/JavaKataTeamOne/KataTest.java
@@ -2,12 +2,20 @@ package edu.pdx.cs410J.JavaKataTeamOne;
 
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class KataTest
 {
 
   @Test
   public void canInstantiateKataClass() {
     new Kata();
+  }
+
+  @Test
+  public void romanNumforOneisI() {
+    assertThat(Kata.romanNumeralFor(1), equalTo("I"));
   }
 
 }
