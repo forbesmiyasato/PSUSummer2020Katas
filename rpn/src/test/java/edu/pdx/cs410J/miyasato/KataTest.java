85d59e58f88a9b95cd8d2dd15b58e1bd86daa4ca
angiemcgraw <mangie@pdx.edu>
2020-07-22 20:39:18 -0700

    update

--- rpn/src/test/java/edu/pdx/cs410J/miyasato/KataTest.java
+++ rpn/src/test/java/edu/pdx/cs410J/miyasato/KataTest.java
@@ -20,4 +20,28 @@ public class KataTest
 
       assertThat(kata.RPN("20", "5", "/"), equalTo(4));
   }
+
+  @Test
+  public void testRPNCalculator2() {
+        Kata kata = new Kata();
+        assertThat(kata.RPN("20", "4", "*"), equalTo(80));
+  }
+
+  @Test
+  public void testRPNCalculator3() {
+        Kata kata = new Kata();
+        assertThat(kata.RPN("20", "4", "+"), equalTo(24));
+  }
+
+  @Test
+  public void testRPNCalculator4() {
+        Kata kata = new Kata();
+        assertThat(kata.RPN("20", "4", "-"), equalTo(16));
+  }
+
+  @Test
+  public void testRPNCalculator5() {
+        Kata kata = new Kata();
+        assertThat(kata.RPN("4", "2", "+", "7", "-"), equalTo(-1));
+  }
 }
