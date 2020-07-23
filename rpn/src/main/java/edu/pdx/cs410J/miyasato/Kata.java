56feb38d604ce749973fe36b15fe85d627d0fa92
Kanra Su <karawakara@gmail.com>
2020-07-22 20:31:48 -0700

    update kata

--- rpn/src/main/java/edu/pdx/cs410J/miyasato/Kata.java
+++ rpn/src/main/java/edu/pdx/cs410J/miyasato/Kata.java
@@ -9,31 +9,65 @@ import java.util.Stack;
  * class (and its tests).
  */
 public class Kata {
-                                                                                    
-
-  public static void main(String[] args) {
-    System.err.println("Missing command line arguments");
-    System.exit(1);
-  }
-
-  public static int RPN(String ...args) {
-    Stack<Integer> stack = new Stack();
-
-    for (int i = 0; i < args.length; i++) {
-      switch (args[i]) {
-        case "/":
-        case "+":
-        case "-":
-        case "*":
-          if (stack.size() < 2) {
-          }
-          break;
-        default:
-          stack.push(Integer.parseInt(args[i]));
-          break;
-      }
-    }
-
-    return 0;
-  }
+	
+	
+	public static void main(String[] args) {
+		System.err.println("Missing command line arguments");
+		System.exit(1);
+	}
+	
+	public static int RPN(String... args) throws IllegalArgumentException {
+		Stack<Integer> stack = new Stack();
+		int result = 0;
+		int first;
+		int second;
+		
+		for (int i = 0; i < args.length; i++) {
+			switch (args[i]) {
+				case "/":
+					if (stack.size() < 2) {
+						throw new IllegalArgumentException("no enough parameters");
+					}
+					second = stack.pop();
+					first = stack.pop();
+					
+					result = first / second;
+					
+					break;
+				case "+":
+					if (stack.size() < 2) {
+						throw new IllegalArgumentException("no enough parameters");
+					}
+					second = stack.pop();
+					first = stack.pop();
+					
+					result = first + second;
+					break;
+				case "-":
+					if (stack.size() < 2) {
+						throw new IllegalArgumentException("no enough parameters");
+					}
+					second = stack.pop();
+					first = stack.pop();
+					
+					result = first - second;
+					break;
+				case "*":
+					if (stack.size() < 2) {
+						throw new IllegalArgumentException("no enough parameters");
+					}
+					second = stack.pop();
+					first = stack.pop();
+					
+					result = first * second;
+					
+					break;
+				default:
+					stack.push(Integer.parseInt(args[i]));
+					break;
+			}
+		}
+		
+		return result;
+	}
 }
